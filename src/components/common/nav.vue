5366b5aa711a9f4c6440bce7ddc0e27b27a24493
kroyo <kroyoshi@163.com>
2018-10-16 17:57:58 +0800

    完善首页样式

--- src/components/common/nav.vue
+++ src/components/common/nav.vue
@@ -18,11 +18,19 @@
       </router-link>
       <router-link tag="a" to="/reward">
         <li>
-          <span class="icon-reward" style="font-size: 16px;vertical-align:middle"></span>
+          <i class="iconfont icon-reward"></i>
           打赏
         </li>
       </router-link>
     </ul>
+    <div class="header-right">
+      <a href="https://github.com/kroyo/website" target="_black">
+        <span class="li-icon" title="github"><i class="iconfont icon-github"></i></span>
+      </a>
+      <router-link tag="a" to="/setting" class="a-icon">
+        <span class="li-icon" title="设置" @click="hideLeftContent"><i class="iconfont icon-setting"></i></span>
+      </router-link>
+    </div>
   </nav>
 </template>
 
@@ -32,13 +40,77 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import url('http://at.alicdn.com/t/font_875913_5p2166y4hd6.css');
+$theme-color: #409EFF;
 .site-header{
-  .header-ul{
-    font-size: 0;
+  position:relative;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  overflow: hidden;
+}
+.header-ul{
+  height: 100%;
+  font-size: 0;
+  padding: 0 20px;
+  a{
+    display: inline-block;
+    height: 100%;
+    font-size: 14px;
+    color: #fff;
+    padding: 0 14px;
+    cursor: pointer;
+    transition: .4s all;
+    &:hover,&.link-active{
+      color: $theme-color;
+      li:after{
+        transform: scale(1,1);
+      }
+    }
   }
   li{
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    &:after{
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      display: block;
+      content: '';
+      width: 100%;
+      height: 2px;
+      background-color: $theme-color;
+      transform: scale(0,1);
+      transform-origin: center;
+      transition: .4s all;
+    }
+  }
+  img{
+    vertical-align: middle;
+  }
+  .logo{
+    padding-left: 0;
+  }
+}
+.header-right{
+  position: absolute;
+  top: 0;
+  right: 10px;
+  height: 40px;
+  font-size: 0;
+  a{
     display: inline-block;
     font-size: 14px;
+    color: #fff;
+    padding: 0 10px;
+    cursor: pointer;
+    span{
+      line-height: 40px;
+    }
+    i{
+      font-size: 20px;
+    }
   }
 }
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home"></div>
+  <main class="home">
+    <footer class="home-footer">
+      每日一图由 时光浅滩 提供 | Copyright &copy; 2018 ZHONGLIEHAO.COM
+    </footer>
+  </main>
 </template>
 
 <script>
@@ -7,4 +11,14 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.home-footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  color: #fff;
+  font-size: 12px;
+  text-align: center;
+}
 </style>
