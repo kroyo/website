3416ee374c7b6367a497fd8cd3ede05724d24ece
kroyo <kroyoshi@163.com>
2019-04-25 17:36:14 +0800

    配置修改

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <site-bg></site-bg>
     <nav-slider></nav-slider>
     <transition name="fade">
-      <router-view/>
+      <router-view class="fade-content" />
     </transition>
   </div>
 </template>
@@ -27,4 +27,12 @@ export default {
   width: 100%;
   height: 100%;
 }
+.fade-content
+  transform:translate3d(0,0,0)
+  transition: .8s all
+  &.fade-enter-to,&.fade-leave-to
+    transition: all 0.8s
+  &.fade-enter,&.fade-leave-to
+    opacity:0
+    transform:translate3d(0,50px,0)
 </style>
